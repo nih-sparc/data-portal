c7b76a064a050e093afe62c4648439f0892e0431
Joost Wagenaar <joost@blackfynn.com>
2019-02-14 09:51:55 -0500

    updated templates

--- dat_core/views.py
+++ dat_core/views.py
@@ -7,9 +7,6 @@
 from app import app
 from flask import render_template, Blueprint
 from logger import logger
-from blackfynn import Blackfynn
- 
-bf = None
 
 ################
 #### config ####
@@ -17,17 +14,6 @@ bf = None
  
 dat_core_blueprint = Blueprint('dat_core', __name__, template_folder='templates')
 
-@app.before_first_request
-def connect_to_blackfynn():
-    global bf
-    #bf = Blackfynn(
-    #    api_token=Config.BLACKFYNN_API_TOKEN,
-    #    api_secret=Config.BLACKFYNN_API_SECRET,
-    #    env_override=False,
-    #    host=Config.BLACKFYNN_API_HOST,
-    #    concepts_api_host=Config.BLACKFYNN_CONCEPTS_API_HOST
-    #)
- 
 ################
 #### routes ####
 ################
