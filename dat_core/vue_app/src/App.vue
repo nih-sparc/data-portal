f5bfb81f254ddec155bbd8a8a3c3ef23d157a011
Joost Wagenaar <joost@blackfynn.com>
2019-02-19 14:07:22 -0500

    remove demo component

--- dat_core/vue_app/src/App.vue
+++ dat_core/vue_app/src/App.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="browse">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <sparc-welcome />
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <!-- <bf-vega id="1" class="list"></bf-vega> -->
 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import BfVega from './components/bf-vega/BfVega.vue'
 import SparcWelcome from './components/sparc-welcome/SparcWelcome.vue'
 
 export default {
   name: 'browse',
   components: {
-    HelloWorld,
     BfVega,
     SparcWelcome
   }
