3cd43293389fd7122d98e8861b8912c057bc8c2e
Joost Wagenaar <joost@blackfynn.com>
2019-02-25 17:19:16 -0500

    added sparc stats, and scss variables

--- dashboard/vue_app/src/App.vue
+++ dashboard/vue_app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="dashboard">
-    <sparc-welcome />
+    <sprc-stats />
 
   </div>
 </template>
@@ -8,12 +8,14 @@
 <script>
 import BfVega from './components/bf-vega/BfVega.vue'
 import SparcWelcome from './components/sparc-welcome/SparcWelcome.vue'
+import SprcStats from './components/SprcStats/SprcStats.vue'
 
 export default {
   name: 'dashboard',
   components: {
     BfVega,
-    SparcWelcome
+    SparcWelcome,
+    SprcStats
   }
 }
 </script>
