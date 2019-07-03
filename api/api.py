037f8ac18831fedc0b4a59803e0df5e3a751ae4a
Jesse Khorasanee <37255664+Tehsurfer@users.noreply.github.com>
2019-07-03 13:23:05 +1200

    remove api connection

--- api/api.py
+++ api/api.py
@@ -28,14 +28,6 @@ ma = Marshmallow(app)
 def connect_to_blackfynn():
     global bf
 
-    bf = Blackfynn(
-       api_token=Config.BLACKFYNN_API_TOKEN,
-       api_secret=Config.BLACKFYNN_API_SECRET,
-       env_override=False,
-       host=Config.BLACKFYNN_API_HOST,
-       concepts_api_host=Config.BLACKFYNN_CONCEPTS_API_HOST
-    )
-
 @app.before_first_request
 def connect_to_graphenedb():
     global gp
