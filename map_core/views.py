e5881fee28ef8fce07860d1a7697b2df70a5f277
alan-wu <alan.wu@auckland.ac.nz>
2019-07-05 15:22:32 +1200

    Update mapcore and fix a bug.

--- map_core/views.py
+++ map_core/views.py
@@ -99,7 +99,7 @@ def map_annotations(map):
     if rows is None:
         annotations = {}
     else:
-        annotations = json.loads(row[0])
+        annotations = json.loads(rows[0])
     return jsonify(annotations)
 
 @map_core_blueprint.route('flatmap/<string:map>/images/<string:image>')
