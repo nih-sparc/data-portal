8e0591f298954ef6f7cda6d468efb7e7236ca08e
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-07-08 12:22:03 +0200

    Added link from detail to search results

--- sim_core/vue_app/src/components/DatasetSearch/DatasetDetail.vue
+++ sim_core/vue_app/src/components/DatasetSearch/DatasetDetail.vue
@@ -1,25 +1,32 @@
 <template>
-  <div v-if="isFetching">
-    Fetching...
-  </div>
-  <div v-else-if="dataset">
-    <el-row :gutter="18">
-      <el-col :sm="8">
-        <el-image :src="dataset.banner" fit="cover" lazy></el-image>
-      </el-col>
-      <el-col :sm="16">
-        <h2>{{ dataset.name }}</h2>
-        <h3><p>{{ dataset.description }}</p></h3>
-        <p>Organization: {{ dataset.organizationName }}</p>
-        <p>Owner: {{ dataset.ownerName }}</p>
-        <p v-if="dataset.contributors.length">
-          Contributors: <span v-for="(contributor, i) in dataset.contributors" :key="contributor">{{ contributor }}{{ i === dataset.contributors.length-1 ? "." : ", "}}</span>
-        </p>
-      </el-col>
-    </el-row>
-  </div>
-  <div v-else>
-    <p>Sorry, we couldn't retreive the dataset for the given id.</p>
+  <div>
+    <p>
+      <router-link to="/">
+        <el-link type="primary" size="small">&lt; Back to search results</el-link>
+      </router-link>
+    </p>
+    <div v-if="isFetching">
+      Fetching...
+    </div>
+    <div v-else-if="dataset">
+      <el-row :gutter="18">
+        <el-col :sm="8">
+          <el-image :src="dataset.banner" fit="cover" lazy></el-image>
+        </el-col>
+        <el-col :sm="16">
+          <h2>{{ dataset.name }}</h2>
+          <h3><p>{{ dataset.description }}</p></h3>
+          <p>Organization: {{ dataset.organizationName }}</p>
+          <p>Owner: {{ dataset.ownerName }}</p>
+          <p v-if="dataset.contributors.length">
+            Contributors: <span v-for="(contributor, i) in dataset.contributors" :key="contributor">{{ contributor }}{{ i === dataset.contributors.length-1 ? "." : ", "}}</span>
+          </p>
+        </el-col>
+      </el-row>
+    </div>
+    <div v-else>
+      <p>Sorry, we couldn't retreive the dataset for the given id.</p>
+    </div>
   </div>
 </template>
 
