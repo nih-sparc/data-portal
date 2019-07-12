3cdc085137f4476197d552d6f2174bbba68c4dca
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-07-13 09:46:49 +1200

    Add proxy routes for scicrunch and biolucida.

--- map_core/views.py
+++ map_core/views.py
@@ -37,18 +37,6 @@ def getModels(p):
     return redirect(url)
 
 
-def get_response_from_remote(url):
-    try:
-        session = requests.Session()
-        r = session.get(url, cookies=request.cookies)
-    except Exception as e:
-        return "proxy service error: " + str(e), 503
-    resp = make_response(r.content)
-    if r.cookies.get('sessionid'):
-        resp.set_cookie('sessionid', r.cookies.get('sessionid'))
-    return resp
-
-
 @map_core_blueprint.route('staging_model/<path:p>')
 def getStagingModel(p):
     url = 'https://staging.physiomeproject.org/workspace/{0}'.format(p)
@@ -135,3 +123,58 @@ def kb_query():
             return jsonify(graph.query(query.get('sparql')))
     except RuntimeError:
         abort(404, 'Cannot open knowledge base')
+
+
+@map_core_blueprint.route('knowledgebase/<path:data_set>')
+def knowledge_base_proxy(data_set=''):
+    query_string = ensure_string(request.query_string)
+    url = 'https://scicrunch.org/api/1/dataservices/federation/data/{0}?{1}'.format(data_set, query_string, 'utf-8')
+    return get_response_from_remote(url)
+
+
+@map_core_blueprint.route('biolucida/<path:api_method>', methods=['GET', 'POST'])
+def biolucida_client_proxy(api_method=''):
+    url = 'https://sparc.biolucida.net/api/v1/{0}'.format(api_method, 'utf-8')
+    if request.method == 'POST':
+        return post_response_from_remote(url, data=json.loads(request.data))
+    else:
+        return get_response_from_remote(url, headers={'token': request.headers['token']})
+
+
+def get_response_from_remote(url, headers=None):
+    try:
+        session = requests.Session()
+        if headers is not None:
+            session.headers.update(headers)
+
+        r = session.get(url, cookies=request.cookies)
+    except Exception as e:
+        return "proxy service error: " + str(e), 503
+    resp = make_response(r.content)
+    if r.cookies.get('sessionid'):
+        resp.set_cookie('sessionid', r.cookies.get('sessionid'))
+    return resp
+
+
+def post_response_from_remote(url, data=None):
+    try:
+        session = requests.Session()
+        r = session.post(url, data=data)
+    except Exception as e:
+        return "proxy service error: " + str(e), 503
+
+    resp = make_response(r.content)
+
+    if r.cookies.get('sessionid'):
+        resp.set_cookie('sessionid', r.cookies.get('sessionid'))
+
+    return resp
+
+
+def ensure_string(data):
+    if type(data) == bytes:
+        string = data.decode()
+    else:
+        string = data
+
+    return string
