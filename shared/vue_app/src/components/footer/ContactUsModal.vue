8780ae35a079fe481eb07828ef1ab01161322462
Maha Zayed <maha@blackfynn.com>
2019-07-18 13:07:53 -0400

    Got contact modal to display, need to connect send button

--- shared/vue_app/src/components/footer/ContactUsModal.vue
+++ shared/vue_app/src/components/footer/ContactUsModal.vue
@@ -1,55 +1,40 @@
 <template>
-     <el-dialog :show-close="false" :visible.sync="visible" @open="resetForm">
-        <bf-dialog-header slot="title" title="Request a 3rd Reviewer"/>
-
-        <dialog-body>
-          <p
-            class="mb-24"
-          >This participant will be blinded, randomized, and set to “In Review”. We’ll also send an email to your reviewer.</p>
-
-          <!-- <el-form
-            ref="adjudicatorForm"
-            :model="adjudicatorForm"
-            :rules="adjudicatorFormRules"
-            label-position="top"
-            @submit.native.prevent="submitAdjudicatorForm"
-          >
-            <el-form-item prop="selectedAdjudicator">
-              <template slot="label">
-                Adjudicator
-                <span class="label-helper">required</span>
-              </template>
-              <el-select
-                v-model="adjudicatorForm.selectedAdjudicator"
-                value-key="id"
-                placeholder="Please choose one"
-              >
-                <el-option
-                  v-for="item in adjudicatorOptions"
-                  :key="item.id"
-                  :label="item.fullName"
-                  :value="item.fullName"
-                />
-              </el-select>
-            </el-form-item>
-          </el-form> -->
-        </dialog-body>
-
-        <div slot="footer" class="dialog-footer">
-          <bf-button class="secondary">Cancel</bf-button>
-
-          <bf-button
-            class="primary"
-
-          >Send</bf-button>
-        </div>
-      </el-dialog>
-
+  <el-dialog :show-close="true" :visible.sync="visible" @open="resetForm">
+    <div class="header">
+       <h2>Send us a Message</h2>
+    </div>
+    <dialog-body>
+      <el-form :label-position="labelPosition" label-width="100px" :model="contactUsForm">
+        <el-form-item label="Your Name">
+          <el-input aria-placeholder="Enter your name" v-model="contactUsForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="Your Email">
+          <el-input aria-placeholder="Enter your email" type="email" v-model="contactUsForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="Your Message">
+          <el-input aria-placeholder="Your message" type="textarea" height="60" v-model="contactUsForm.message"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="send-button">Send</el-button>
+        </el-form-item>
+      </el-form>
+    </dialog-body>
+  </el-dialog>
 </template>
 
 <script>
     export default {
         name: 'ContactUsModal',
+        data() {
+          return {
+            labelPosition: 'top',
+            contactUsForm: {
+              name: '',
+              email: '',
+              message: ''
+            }
+          }
+        },
         props: {
             visible: {
                 type: Boolean,
@@ -61,4 +46,64 @@
 
 <style lang="scss" scoped>
 
+.send-button {
+  display: flex;
+  justify-content: center;
+  background-color: #24245b;
+  color: white;
+  height: 40px;
+  width: 128px;
+  border-radius: 4px;
+  margin-bottom: 148px;
+  margin-left: 121px;
+}
+
+.header {
+  margin-left: -15px;
+}
+
+h2 {
+  color: #24245b;
+  font-weight: normal;
+  font-size: 46px;
+  line-height: 46px;
+  padding-top: 148px;
+  padding-bottom: 32px;
+  width: 117%;
+}
+
+/deep/ .el-dialog {
+  width: 1280px;
+  height: 800px;
+
+  .el-dialog__body {
+    background-color: #EDF1FC;
+    padding-left: 441px;
+    padding-right: 462px;
+  }
+
+  .el-dialog__header {
+    padding: 0;
+  }
+
+  .el-dialog__headerbtn .el-dialog__close {
+    font-size: 34px;
+    color: #24245b;
+    font-weight: bold;
+  }
+}
+
+/deep/ .el-input {
+  .el-input__inner {
+    border-radius: 4px;
+    border: 1px solid #909399;
+  }
+}
+
+/deep/ .el-textarea {
+  .el-textarea__inner {
+    border-radius: 4px;
+    border: 1px solid #909399;
+  }
+}
 </style>
