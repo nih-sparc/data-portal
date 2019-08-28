ee7db40b6013cdae4eb73a7676e212ead6b7cb7d
Maha Zayed <maha@blackfynn.com>
2019-08-28 15:48:49 -0400

    Added link to logo

--- shared/vue_app/src/components/logo/SparcLogo.vue
+++ shared/vue_app/src/components/logo/SparcLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <img class="logo" alt="SPARC logo" :src="logo"/>
+    <a href="https://data.sparc.science"><img class="logo" alt="SPARC logo" :src="logo"/></a>
 </template>
 
 <script>
