6ea469cd1c47e350c0a1410c393f0b2b0b239eaf
Maha Zayed <maha@blackfynn.com>
2019-09-09 14:17:36 -0400

    Added logic to open MS Word files on browse

--- dat_core/vue_app/src/components/browse/Browse.vue
+++ dat_core/vue_app/src/components/browse/Browse.vue
@@ -294,6 +294,7 @@ export default {
       const fileName = pathOr('', ['row', 'name'], scope)
 
       const requestUrl = `/api/download?key=${filePath}`
+
       this.$http.get(requestUrl).then(
         response => {
           this.downloadFile(fileName, response.data)
@@ -307,7 +308,26 @@ export default {
      * @param {Object} scope
      */
     openFile: function(scope) {
-      
+      const filePath = compose(
+        last,
+        defaultTo([]),
+        split('s3://blackfynn-discover-use1/'),
+        pathOr('', ['row', 'uri']),
+      )(scope)
+
+      const fileName = pathOr('', ['row', 'name'], scope)
+
+      const requestUrl = `/api/download?key=${filePath}`
+
+      this.$http.get(requestUrl).then(
+        response => {
+          const url = response.data
+          const encodedUrl = encodeURIComponent(url)
+          const finalURL = `https://view.officeapps.live.com/op/view.aspx?src=${encodedUrl}`
+          window.open(finalURL, '_blank')
+        }
+      )
+
     },
 
     /**
